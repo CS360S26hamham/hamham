1d8de009a151556fbbcfa60b3a34bd98619b583d
Mina Zeeshan <27100067@lums.edu.pk>
2026-02-17 11:56:41 +0500

    circle class

---
+++ app/src/main/java/com/example/hamham/Circle.java
@@ -0,0 +1,17 @@
+package com.example.hamham;
+
+public class Circle {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
